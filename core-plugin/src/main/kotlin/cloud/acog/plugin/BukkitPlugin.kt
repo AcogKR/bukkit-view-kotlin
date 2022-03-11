32018a3a2c59b7e2a648124a1f617a40ee3cc124
acog <acog.seongmin@gmail.com>
2022-03-11 23:13:05 +0900

    BukkitView Kotlin DSL

--- core-plugin/src/main/kotlin/cloud/acog/plugin/BukkitPlugin.kt
+++ core-plugin/src/main/kotlin/cloud/acog/plugin/BukkitPlugin.kt
@@ -15,35 +15,12 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class BukkitPlugin : JavaPlugin() {
 
-    override fun onDisable() {
-        super.onDisable()
-    }
-
     override fun onEnable() {
         BukkitView.register(this)
     }
 
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        val player = sender as Player 
-        if (player.isOp) {
-            pageViewLayout("Players", 6) {
-                contents = Bukkit.getOnlinePlayers().map { player ->
-                    pageViewItem(getPlayerHead(player)) {
-                        clicker.sendMessage("Hello World!")
-                        ViewAction.CLOSE
-                    }
-                }.toMutableList()
-            }.toView(1).openView(player, this)
-        }
-        return false
-    }
 
-
-    fun getPlayerHead(player: Player) : ItemStack {
-        return ItemStack(Material.PLAYER_HEAD).apply {
-            val meta: SkullMeta = itemMeta as SkullMeta
-            meta.owningPlayer = player
-            itemMeta = meta
-        }
+    override fun onDisable() {
+        super.onDisable()
     }
 }
