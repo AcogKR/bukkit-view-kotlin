2934737de5bbc1f7fc707bd9dbe7e716eac6f6cf
acog <acog.seongmin@gmail.com>
2022-03-12 20:13:58 +0900

    BukkitView Kotlin DSL

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     compileOnly(spigot("1.18.1"))
     implementation(kotlin("stdlib"))
-    implementation("io.typecraft:bukkit-view-core:${version}")
+    api("io.typecraft:bukkit-view-core:${version}")
 }
 
 publishing {
@@ -29,3 +29,55 @@ publishing {
         }
     }
 }
+
+/*
+publishing {
+    publications {
+        create("spigradleAnnotations", MavenPublication::class) {
+            from(components["java"])
+            // NOTE: https://central.sonatype.org/publish/requirements/#sufficient-metadata
+            pom {
+                name.set("${project.group}:${project.name}")
+                description.set("Annotations and processors for specify the main class.")
+                url.set("https://github.com/AcogKR/bukkit-view-kotlin")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Acogkr")
+                        name.set("Seongmin Hong")
+                        email.set("acog.seongmin@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/AcogKR/bukkit-view-kotlin.git")
+                    developerConnection.set("scm:git:ssh://github.com:AcogKR/bukkit-view-kotlin.git")
+                    url.set("https://github.com/AcogKR/bukkit-view-kotlin/tree/master")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "sonatypeReleases"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = findProperty("ossrhUsername")?.toString()
+                password = findProperty("ossrhPassword")?.toString()
+            }
+        }
+        maven {
+            name = "sonatypeSnapshots"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = findProperty("ossrhUsername")?.toString()
+                password = findProperty("ossrhPassword")?.toString()
+            }
+        }
+    }
+}
+*/
