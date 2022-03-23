1fa9b930b83024c590127fcc3263af503dd50cea
acog <acog.seongmin@gmail.com>
2022-03-23 10:39:44 +0900

    refactor!: new ChestView

--- plugin/src/main/kotlin/cloud/acog/plugin/BukkitPlugin.kt
+++ plugin/src/main/kotlin/cloud/acog/plugin/BukkitPlugin.kt
@@ -2,7 +2,10 @@ package cloud.acog.plugin
 
 import cloud.acog.bukkitview.kotlin.*
 import io.typecraft.bukkit.view.BukkitView
+import io.typecraft.bukkit.view.ChestView
 import io.typecraft.bukkit.view.ViewAction
+import io.typecraft.bukkit.view.page.PageContext
+import io.typecraft.bukkit.view.page.PageViewControl
 import io.typecraft.bukkit.view.page.PageViewLayout
 import org.bukkit.Bukkit
 import org.bukkit.Material
@@ -12,6 +15,8 @@ import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.Arrays
+import java.util.function.Function
 
 class BukkitPlugin : JavaPlugin(){
 
@@ -20,22 +25,41 @@ class BukkitPlugin : JavaPlugin(){
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        getPlayerHeadPage().toView(1).openView(sender as Player, this)
-        return super.onCommand(sender, command, label, args)
+        val player: Player = if (sender is Player && sender.isOp) sender else return false
+        val head: String = if (args.isEmpty()) "" else args[0]
+        when (head) {
+            "page" -> createMyViewLayout().toView(1).openView(player, this)
+            "chest" -> createChestView().openView(player, this)
+            else -> listOf(
+                "§a/${label} page: §fshow a demo pagination view.",
+                "§a/${label} chest: §fshow a demo chest view."
+            ).forEach(player::sendMessage)
+        }
+        return true
     }
 
-    private fun getPlayerHeadPage() : PageViewLayout {
+    private fun createChestView() : ChestView {
+        return chestView("test", 6) {
+            val wallItem = justViewItem(Material.BLACK_STAINED_GLASS.toItem(1))
+            for (i in 0 until 9) {
+                controls[i] = wallItem
+            }
+
+            controls[8] = viewItem(simpleItem(Material.BARRIER) {
+                setDisplayName("§cEXIT")
+            }) { ViewAction.CLOSE }
+        }
+    }
+    private fun createMyViewLayout() : PageViewLayout {
         return pageViewLayout("Player Heads", 6) {
-            contents = Bukkit.getOnlinePlayers().map { players ->
-                pageViewItem(simpleItem(Material.PLAYER_HEAD) {
-                    simpleMetaOf<SkullMeta> {
-                        owningPlayer = players
-                        setDisplayName(players.name)
+            contents = Material.values()
+                .filter { mat -> mat.isItem && !mat.isAir }
+                .map { material -> pageViewItem(material.toItem(1)) {
+                    val player: Player = clicker
+                    if (player.isOp) {
+                        clicker.inventory.addItem(material.toItem(1))
                     }
-                }) {
-                    if (click.isLeftClick) clicker.teleport(players.location)
-                    else if (click.isRightClick) players.kickPlayer("?")
-                    ViewAction.CLOSE
+                    ViewAction.NOTHING
                 }
             }.toMutableList()
         }
